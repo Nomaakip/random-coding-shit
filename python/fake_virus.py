973d9e79b233b278ac1b1632d92495e4d465a27f
hacks.guide <128547338+Nomaakip@users.noreply.github.com>
2025-02-27 12:02:34 +0100

    Update fake_virus.py

--- python/fake_virus.py
+++ python/fake_virus.py
@@ -1,24 +1,82 @@
 import pyautogui
-import time
+import webbrowser
+import random
 import requests
+import psutil
 import os
 
 
-# download the fucking miku imagee
+searches = [
+    'fuck java',
+    'how to trick an idiot',
+    'why is my microwave laughing',
+    'can I sue my toaster',
+    'is water wet or am I just dumb',
+    'free robux no scam',
+    'how to become sigma in 5 minutes',
+    'why does my cat pay taxes',
+    'best excuses to not pay taxes',
+    'can AI legally adopt me',
+    'how to delete Internet Explorer in real life',
+    'greg is watching me help',
+    'how to summon hatsune miku at 3am',
+    'free vbucks generator real 2025',
+    'can I sell my browser history on eBay',
+    'do penguins have knees',
+    'why does my keyboard smell like Doritos',
+    'how to install Windows on my fridge',
+    'can I drink liquid nitrogen for fun',
+    'why does my Roomba know my name',
+    'how to become CEO of Google with no experience',
+    'best way to get banned from roblox',
+    'where to download RAM for free',
+    'how to legally marry my anime waifu',
+    'help my smart fridge joined a cult',
+    'why does my wifi connect to the backrooms',
+    'can you deep fry air',
+    'do I really need all my organs'
+]
+
+# download the fucking miku image
 url = 'https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/b8/92/ce/b892cead-70a1-8f29-0045-af30080d3a16/4571640501760_cover.png/316x316bb.webp'
-
-response = requests.get(url)
-
-with open("nice_try.webp", "wb") as file:
-        file.write(response.content)
 
 # get the username
 username = os.getlogin()
 
-while True:
-    print(f'Hi, {username}.')
-    pyautogui.press('capslock')
-    pyautogui.write('hacked by fake virus? imagine!1!1!!')
-    pyautogui.press('enter')
-    os.startfile("nice_try.webp")
-    time.sleep(0.1)
+
+def main():
+    downloaded_number = 1
+    while True:
+        
+        ram = psutil.virtual_memory()
+        ram_percentage = ram.percent
+        max_ram = 85
+        
+        if ram_percentage >= max_ram:
+            break
+        
+        # download the image
+        response = requests.get(url)
+        with open("nice_try.webp", "wb") as file:
+            file.write(response.content)
+
+        # print the username
+        print(f'Hi, {username}.')
+        
+        # press shit idfk
+        pyautogui.press('capslock')
+        pyautogui.typewrite('hacked by fake_virus...imagine...')
+        pyautogui.press('enter')
+        
+        
+        new_filename = f"nice_try{downloaded_number}.webp"
+        
+        downloaded_number += 1
+        os.rename("nice_try.webp", new_filename)
+        os.startfile(new_filename)
+        
+        search = random.choice(searches)
+        webbrowser.open(f"https://www.google.com/search?q={search}")
+        webbrowser.open(f"https://www.youtube.com/watch?v=MX8eYdEOU5w")
+    
+main()
